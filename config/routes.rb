e952d9eb481cf853ae4aeeb281d830362677b9fd
Pierre Andignac <pierreandignac.work@gmail.com>
2024-09-11 21:59:44 +0200

    adding routes controllers and views for bio and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/bio'
+  get 'pages/contact'
   get 'albums/index'
   get 'albums/show'
   get 'photos/index'
@@ -6,7 +8,9 @@ Rails.application.routes.draw do
   resources :albums, only: [:index, :show] do
     resources :photos, only: [:index, :show]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get 'bio', to: 'pages#bio'
+  get 'info', to: 'pages#contact'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
